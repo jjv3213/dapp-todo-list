6b40df4440b50df8588ee13ead44182fce9723c6
joelv <joelv3213@gmail.com>
2019-06-11 11:58:44 -0600

    install web3 and connect with provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
 
 function App() {
+  const [account, setAccount] = useState("");
+
+  const loadBlockChainData = async () => {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+  };
+
+  useEffect(() => {
+    loadBlockChainData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Welcome....</h1>
+      <p>Your account: {account}</p>
     </div>
   );
 }
